d44f1e1eb0071741275c91034fd8b309df13562b
vimba <vimbamzaza@gmail.com>
2026-09-09 14:17:09 +0200

    imported style sheets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import LoginPage from './pages/LoginPage.jsx'
 import TripMapPage from "./pages/TripMapPage.jsx"
+import './index.css'
+import 'leaflet/dist/leaflet.css'
 
 function App() {
 
